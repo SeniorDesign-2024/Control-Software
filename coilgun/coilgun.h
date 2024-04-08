eb2f29edcf66dcf3ad24b44f6e902fc95dfb7749
amwcandy <alex.m.wortmann@gmail.com>
2024-04-08 00:50:04 -0500

    testbench code

--- coilgun/coilgun.h
+++ coilgun/coilgun.h
@@ -2,13 +2,15 @@
 #define COILGUN_H
 
 #include <arduino.h>
+#include <string.h>
+#include <Servo.h>
+#include <Adafruit_NeoPixel.h>
 
 bool SAFETY_STATE =      0; // 0 = safe (discharge gun), 1 = fire (normal operation)
-#define COIL_NUM                  2
-#define VOLT_ADC_MIN              30
-#define VOLT_ADC_MAX              1023
-const int MAX_VOLT =            400;
-const int TARGET_VOLT =         350;
+#define COIL_NUM                  6
+#define VOLT_ADC_MIN              0
+#define VOLT_ADC_MAX              1024
+const int MAX_VOLT =            350;
 const int MIN_VOLT =            0;
 
 const int REFRESH_TIME = 1000;            // time to refresh the Nextion page every 100 ms
@@ -17,26 +19,42 @@ const int REFRESH_TIME2 = 1000;           // time to refresh the Nextion page ev
 unsigned long refresh_timer2 = millis(); // timer for refreshing Nextion's page
 
 // -------- PIN DEFINITIONS --------
-#define CHARGING_LED        5
-#define CHARGED_LED         8
-#define DISCHARGING_LED     11
-#define CHARGE_ENABLE       15
-#define DISCHARGE_ENABLE    17
-#define OPTICAL_SENS_1      23
-#define OPTICAL_SENS_2      22
-#define TRIGGER             2
-#define SAFETY_SWITCH       3
+
+//Charge Board
+#define CHARGE_TOGGLE       2
+#define FAULT               3
+#define DONE                4
+
+//Relay Board
+#define DISCHARGE_TOGGLE    11
+#define VOLTAGE_SENS        14
+#define RELAY_TOGGLE        12
+
+//HMI and other
+#define TRIGGER             16
+#define SAFETY_SWITCH       15
 #define LCD_SERIAL          Serial1
 #define LCD_TX              1
 #define LCD_RX              0
-#define VOLTAGE_SENS        20
-#define COIL1               21
-#define COIL2               18
+#define LED_DATA            7
+#define MAX_BRIGHTNESS      70
+#define LED_COUNT           3
+Adafruit_NeoPixel neoPixel = Adafruit_NeoPixel(LED_COUNT, LED_DATA);
+#define SERVO               8
+Servo fire_servo;
+
+
+//Coil Boards
+#define COIL1               22
+#define COIL2               21
+#define COIL3               20
+#define COIL4               19
+#define COIL5               18
+#define COIL6               17
 
 void interrupt();
 void charge();
-void discharge(bool fullDischarge);
-void Edischarge();
+void discharge();
 void coilFire();
 int readVoltage();
 void LCDRead();
--- coilgun/coilgun.ino
+++ coilgun/coilgun.ino
@@ -1,66 +1,86 @@
 #include "coilgun.h"
-#include <string.h>
 
 
 
 void setup ()
 {
-  pinMode(CHARGING_LED, OUTPUT);
-  digitalWrite(CHARGING_LED, LOW);
+  pinMode(CHARGE_TOGGLE, OUTPUT);
+  digitalWrite(CHARGE_TOGGLE, LOW);
+  
+  pinMode(FAULT, INPUT);
+  
+  pinMode(DONE, INPUT);
 
-  pinMode(CHARGED_LED, OUTPUT);
-  digitalWrite(CHARGED_LED, LOW);
+  pinMode(DISCHARGE_TOGGLE, OUTPUT);
+  digitalWrite(DISCHARGE_TOGGLE, LOW);
 
-  pinMode(DISCHARGING_LED, OUTPUT);
-  digitalWrite(DISCHARGING_LED, LOW);
-
-  pinMode(CHARGE_ENABLE, OUTPUT);
-  digitalWrite(CHARGE_ENABLE, LOW);
-
-  pinMode(DISCHARGE_ENABLE, OUTPUT);
-  digitalWrite(DISCHARGE_ENABLE, LOW);
-
-  pinMode(OPTICAL_SENS_1, INPUT_PULLUP);
-
-  pinMode(OPTICAL_SENS_2, INPUT_PULLUP);
-
+  pinMode(VOLTAGE_SENS, INPUT);
+  
+  pinMode(RELAY_TOGGLE, OUTPUT);
+  digitalWrite(RELAY_TOGGLE, LOW);
+  
   pinMode(TRIGGER, INPUT_PULLUP);
 
   pinMode(SAFETY_SWITCH, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(SAFETY_SWITCH), interrupt, FALLING);
 
-  Serial1.begin(115200);
-
-  pinMode(VOLTAGE_SENS, INPUT);
-
+  //Serial1.begin(115200);
+  //fire_servo.attach(SERVO, 0, 2500);
+  //neoPixel.begin();
+  //neoPixel.setBrightness(MAX_BRIGHTNESS);
+  
   pinMode(COIL1, OUTPUT);
   digitalWrite(COIL1, LOW);
 
-  pinMode(COIL2, OUTPUT);
-  digitalWrite(COIL2, LOW);
+  pinMode(COIL2, INPUT_PULLUP);
+  //digitalWrite(COIL2, LOW);
+  
+  pinMode(COIL3, OUTPUT);
+  digitalWrite(COIL3, LOW);
+
+  pinMode(COIL4, OUTPUT);
+  digitalWrite(COIL4, LOW);
+  
+  pinMode(COIL5, OUTPUT);
+  digitalWrite(COIL5, LOW);
+
+  pinMode(COIL6, OUTPUT);
+  digitalWrite(COIL6, LOW);
 
   Serial.begin(115200); 
 }
 
-void interrupt()
-{
-    Serial.println("interrupt");
-    digitalWrite(CHARGING_LED, LOW);
-    digitalWrite(CHARGED_LED, LOW);
-    digitalWrite(DISCHARGING_LED, LOW);
-    digitalWrite(CHARGE_ENABLE, LOW);
-    digitalWrite(DISCHARGE_ENABLE, LOW);
-    Serial1.print("t3.bco=63488");
-    Serial1.print("\xFF\xFF\xFF");
-    Edischarge();
-  SAFETY_STATE = 0;
-}
-
-
-
 void loop ()
 {
-  
+  if (digitalRead(TRIGGER) != 1)
+  {
+    delay(5);
+    while(digitalRead(TRIGGER) != 1)
+    {}
+    Serial.println("FIRE");
+    digitalWrite(COIL1, HIGH);
+    Serial.println("HIGH");
+    delayMicroseconds(40);
+    digitalWrite(COIL1, LOW);
+    Serial.println("LOW");
+  }
+  if(!digitalRead(SAFETY_SWITCH))
+  {
+    digitalWrite(CHARGE_TOGGLE, HIGH);
+    while(!digitalRead(SAFETY_SWITCH));   
+    digitalWrite(CHARGE_TOGGLE, LOW);
+  }
+  if(!digitalRead(COIL2))
+  {
+    while(!digitalRead(COIL2))
+    {
+      digitalWrite(CHARGE_TOGGLE, LOW);
+      digitalWrite(DISCHARGE_TOGGLE, HIGH);
+    }
+    
+    digitalWrite(DISCHARGE_TOGGLE, LOW);
+  }
+}
+  /* this code for actual gun
   int volt = readVoltage();                          // a variable to store the reading
 
   if (!digitalRead(SAFETY_SWITCH))
@@ -71,60 +91,21 @@ void loop ()
   }
   else if (digitalRead(SAFETY_SWITCH))
   {
-    digitalWrite(CHARGING_LED, LOW);
-    digitalWrite(CHARGED_LED, LOW);
-    digitalWrite(DISCHARGING_LED, LOW);
-    digitalWrite(CHARGE_ENABLE, LOW);
-    digitalWrite(DISCHARGE_ENABLE, LOW);
     Serial1.print("t3.bco=63488");
     Serial1.print("\xFF\xFF\xFF");
-    Edischarge();
+    discharge();
     SAFETY_STATE = 0;
   }
   if (SAFETY_STATE)
-  {
-    if (volt>=TARGET_VOLT)
-      digitalWrite(CHARGED_LED, HIGH);
-    else
-      digitalWrite(CHARGED_LED, LOW);
-
-    
+  {    
     bool trigger_state = digitalRead(TRIGGER);
-    if (!trigger_state && (volt>=TARGET_VOLT))
+    if (!trigger_state)
     {
       coilFire();
     }
     LCDRead();
     LCDWrite();
-
-      
-    
-    //charge();
-   // discharge(false);
-    //if (readVoltage()>30)
-    //  discharge(true);
   }
-  
-  /*  LED Test Code
-  digitalWrite(COIL1, HIGH);
-  digitalWrite(COIL2, HIGH);
-  digitalWrite(CHARGING_LED, HIGH);
-  digitalWrite(CHARGED_LED, HIGH);
-  digitalWrite(DISCHARING_LED, HIGH);
-  digitalWrite(CHARGE_ENABLE, HIGH);
-  digitalWrite(DISCHARGE_ENABLE, HIGH);
-  delay(1000);
-  Serial.println("ON");
-  digitalWrite(COIL1, LOW);
-  digitalWrite(COIL2, LOW);
-  digitalWrite(CHARGING_LED, LOW);
-  digitalWrite(CHARGED_LED, LOW);
-  digitalWrite(DISCHARING_LED, LOW);
-  digitalWrite(CHARGE_ENABLE, LOW);
-  digitalWrite(DISCHARGE_ENABLE, LOW);
-  delay(1000);
-  Serial.println("OFF");
-  */
 }
 
 
@@ -138,46 +119,37 @@ void charge()
     LCDWrite();
     if (digitalRead(SAFETY_SWITCH))
     {
-      digitalWrite(CHARGING_LED, LOW);
-      digitalWrite(CHARGED_LED, LOW);
-      digitalWrite(DISCHARGING_LED, LOW);
-      digitalWrite(CHARGE_ENABLE, LOW);
-      digitalWrite(DISCHARGE_ENABLE, LOW);
+      digitalWrite(CHARGE_TOGGLE, LOW);
+      digitalWrite(DISCHARGE_TOGGLE, LOW);
       Serial1.print("t3.bco=63488");
       Serial1.print("\xFF\xFF\xFF");
-      Edischarge();
+      discharge();
       SAFETY_STATE = 0;
     }
     if (SAFETY_STATE == 0)
     {
-      digitalWrite(CHARGING_LED, LOW);
-      digitalWrite(CHARGE_ENABLE, LOW);
+      digitalWrite(CHARGE_TOGGLE, LOW);
+      charging = false;
       return;
     }
     
     voltage = readVoltage();
+
     if (voltage >= MAX_VOLT)
     {
-      discharge(false);
-      return;
-    }
-
-    if (voltage >= TARGET_VOLT)
-    {
-      digitalWrite(CHARGING_LED, LOW);
-      digitalWrite(CHARGE_ENABLE, LOW);
+      digitalWrite(CHARGE_TOGGLE, LOW);
+      charging = false;
       return;
     } 
     else if (!charging) 
     {
-      digitalWrite(CHARGING_LED, HIGH);
-      digitalWrite(CHARGE_ENABLE, HIGH);
+      digitalWrite(CHARGE_TOGGLE, HIGH);
       charging = true;
     }
   }
 }
 
-void discharge(bool fullDischarge)
+void discharge()
 {
   bool discharging = false;
   int voltage = 0;
@@ -187,100 +159,67 @@ void discharge(bool fullDischarge)
     LCDWrite();
     if (digitalRead(SAFETY_SWITCH))
     {
-      digitalWrite(CHARGING_LED, LOW);
-      digitalWrite(CHARGED_LED, LOW);
-      digitalWrite(DISCHARGING_LED, LOW);
-      digitalWrite(CHARGE_ENABLE, LOW);
-      digitalWrite(DISCHARGE_ENABLE, LOW);
+      digitalWrite(CHARGE_TOGGLE, LOW);
+      digitalWrite(DISCHARGE_TOGGLE, LOW);
       Serial1.print("t3.bco=63488");
       Serial1.print("\xFF\xFF\xFF");
-      Edischarge();
+      discharge();
       SAFETY_STATE = 0;
     }
     if (SAFETY_STATE == 0)
     {
-      digitalWrite(DISCHARGING_LED, LOW);
-      digitalWrite(DISCHARGE_ENABLE, LOW);
+      digitalWrite(DISCHARGE_TOGGLE, LOW);
       return;
     }
     
     voltage = readVoltage();
-    if (fullDischarge)  // discharge to 0
-    {
-      if(!discharging)
-      {
-        digitalWrite(DISCHARGING_LED, HIGH);
-        digitalWrite(DISCHARGE_ENABLE, HIGH);
-        discharging = true;
-      }
-      if (voltage <= (MIN_VOLT+10))
-      {
-        digitalWrite(DISCHARGING_LED, LOW);
-        digitalWrite(DISCHARGE_ENABLE, LOW);
-        return;
-      } 
-    }
-    else if (!fullDischarge) // go to target voltage in case of over charging
-    {
-      if(!discharging)
-      {
-        digitalWrite(DISCHARGING_LED, HIGH);
-        digitalWrite(DISCHARGE_ENABLE, HIGH);
-        discharging = true;
-      }
-      else if (voltage <= TARGET_VOLT)
-      {
-        digitalWrite(DISCHARGING_LED, LOW);
-        digitalWrite(DISCHARGE_ENABLE, LOW);
-        return;
-      } 
-    }
-    
-  }
-}
-
-void Edischarge()
-{
-  bool discharging = false;
-  int voltage = 0;
-  
-  while(1)
-  {
-    LCDWrite();
-    voltage = readVoltage();
     if(!discharging)
     {
-      digitalWrite(DISCHARGING_LED, HIGH);
-      digitalWrite(DISCHARGE_ENABLE, HIGH);
+      digitalWrite(DISCHARGE_TOGGLE, HIGH);
       discharging = true;
     }
     if (voltage <= (MIN_VOLT+10))
     {
-      digitalWrite(DISCHARGING_LED, LOW);
-      digitalWrite(DISCHARGE_ENABLE, LOW);
+      digitalWrite(DISCHARGE_TOGGLE, LOW);
       return;
-    } 
+    }    
   }
-  
 }
-
+ 
 void coilFire()
 {
   digitalWrite(COIL1, HIGH);
-  while(digitalRead(OPTICAL_SENS_1));
+  delayMicroseconds(3);
   digitalWrite(COIL1, LOW);
-  digitalWrite(COIL2, HIGH);
-  while(digitalRead(OPTICAL_SENS_2));
-  digitalWrite(COIL2, LOW);
-}
 
+  digitalWrite(COIL2, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(COIL2, LOW);
+
+  digitalWrite(COIL3, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(COIL3, LOW);
+
+  digitalWrite(COIL4, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(COIL4, LOW);
+
+  digitalWrite(COIL5, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(COIL5, LOW);
+
+  digitalWrite(COIL6, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(COIL6, LOW);
+}
+*/
 int readVoltage()
 {
   int voltage = 0;
   voltage = map(analogRead(VOLTAGE_SENS), VOLT_ADC_MIN, VOLT_ADC_MAX, MIN_VOLT, MAX_VOLT);
   return voltage;
 }
-
+/*
 void LCDRead()
 {
   String command;
@@ -295,7 +234,7 @@ void LCDRead()
 
       if (command.equals("DIS"))
       {
-        discharge(true);
+        discharge();
       }
       if (command.equals("CHG"))
       {
@@ -308,25 +247,11 @@ void LCDRead()
 void LCDWrite()
 {
   if ((millis() - refresh_timer) > REFRESH_TIME)
-  { // IMPORTANT do not have serial print commands in the loop without a delay
-    //  or an if statement with a timer condition like this one.
-
-    int volt = analogRead(VOLTAGE_SENS);                // Read the analog pin
-    volt = map(volt, 0, 1022, 0, 400);    // same like: voltage = analogRead(A0)*5000/1024
-    //voltagebar = map(volt, 0, 1022, 0, 400); // same like: voltage = analogRead(A0)*5000/1024
-    /* We Re-map the value of volt from 0-1024 (steps) to 0-5000 millivolt
-    * connect the pins of a Potentiometer on A0 pin, 5v (5000 millivolt) and GND. Outer pins to 5v and GND, middle pin to A0
-    * https://www.arduino.cc/en/tutorial/potentiometer
-    * Turn it over and read values from 0 to 5000 millivolts
-    */
+  { 
+    int volt = readVoltage();
     Serial1.print("n0.val=");
     Serial1.print(volt);
     Serial1.print("\xFF\xFF\xFF");
-    //Serial1.print("j0.val=");
-    //Serial1.print(voltagebar);
-    //Serial1.print("\xFF\xFF\xFF");     
-
-
-    refresh_timer = millis(); // Set the timer equal to millis, create a time stamp to start over the "delay"
+    refresh_timer = millis();
   }
-}
+}*/
